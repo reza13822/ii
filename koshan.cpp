8447dc61087b38a79ea27460725fa35bede9c22d
reza13822 <119834534+reza13822@users.noreply.github.com>
2022-12-13 18:00:53 +0330

    n

---
+++ koshan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+double num[];
+ int main(){
+    int  i = 0 , a , b , c;
+cin >> a;
+for(i ; i < a ; i++)
+{
+    cin >> b;
+    num[i] = b; 
+} cin >> c;
+    for(i , i < a , i++)
+    {
+        if(num[i] == c){
+            cout << i+1 << " ";
+        }
+        else{
+             
+        }
+    }
+ }
